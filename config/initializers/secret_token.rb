ef5ba33e681eb53f2ee6e3024c5638a7f935b17b
vered bauer <veredbr2000@yahoo.com>
2013-02-26 10:44:43 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HitsApp::Application.config.secret_token = '8ea2d934b9288195c7f9a9099a3b58858f3f754d344ed60392fe61ffff79d5ad5fae33c867bd6e5943c4db58dd010032191cfa288fa45c22e4ad730180150626'
